4f3c3ab55b6cbafd722acf6280b1dc8a5ec70802
Abdul Quddous <quddous2369@gmail.com>
2026-03-08 10:07:39 +0500

    Replace email and password with placeholders
    Updated email credentials to placeholders for security.

--- Email_Setup_project/Email_Setup_project/settings.py
+++ Email_Setup_project/Email_Setup_project/settings.py
@@ -124,7 +124,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'quddous97@gmail.com'
-EMAIL_HOST_PASSWORD = 'rnla ybjb hmfu cdjl'
+EMAIL_HOST_USER = 'your email'
+EMAIL_HOST_PASSWORD = 'Your password'
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
